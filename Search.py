e809715345aca22d5eccbf95c710c859da58c345
Chris Andre <chrisandre01@gmail.com>
2015-05-06 23:02:44 -0400

    logic error in rootfinder

--- Search.py
+++ Search.py
@@ -10,7 +10,7 @@ def searchDT(expsinclass, dT, mu):
     c = b - (tof_b - dT) * (b - a) / (tof_b - tof_a)
     tof_c = expsinclass.createExpoSin(c).dT(expsinclass.psi, mu)
 
-    while math.fabs(tof_c - dT) < 1.0e-6:
+    while math.fabs(tof_c - dT) > 1.0e-6:
         if tof_a * tof_c > 0: #same sign
             a = c
         elif tof_b * tof_c > 0: #same sign
